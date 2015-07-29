5a6a953003580f58ac6d32a617cae55f475ac46d
Amit <Amit@Amit-PrimaryBTE.local>
2015-07-28 20:24:55 -0700

    vitals registration

--- BOX/src/com/boxtoeat/services/VitalsRegistration.java
+++ BOX/src/com/boxtoeat/services/VitalsRegistration.java
@@ -29,10 +29,8 @@ import com.twilio.sdk.resource.instance.Message;
 
 
 @Path("/chefRegister")
-public class ChefRegistration {
-	  // Find your Account Sid and Token at twilio.com/user/account
-	  public static final String ACCOUNT_SID = "AC730d289b9395275587fb6e302f6e5fcf";
-	  public static final String AUTH_TOKEN = "5a90f2bbeb958ccf0edc5c715875eee5";
+public class VitalsRegistration {
+	 
 	@POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
 	 public Response uploadFile(
@@ -42,8 +40,8 @@ public class ChefRegistration {
         @FormDataParam("lastName") String lastName , @FormDataParam("addressline1") String addressline1,@FormDataParam("addressline2") String addressline2,
         @FormDataParam("city") String city , @FormDataParam("state") String state,@FormDataParam("zip") long zip,@FormDataParam("phone") long telephone   	
     		) {
-			System.out.println("Hellow"+telephone);
-			// save it
+			
+			// save it to the database
 			writeToDB(uploadedInputStream, fileDetail,username,password,firstName,lastName,addressline1,addressline2,city,zip,state,email,telephone);
 			addPhoneverification(username, telephone);
 			addEmailverification(username,email);
@@ -106,20 +104,11 @@ public class ChefRegistration {
 	
 	private void addPhoneverification(String username,long telephone)
 	{
-		new PhoneVerificationCodeHelper().
+		new PhoneVerificationCodeHelper().createVerificationCode(username, telephone);
 	}
 	
 	
 	
-	public  int generateRandomNumber(int min, int max) {
 
-	   
-	    Random rand = new Random();
-
-	    
-	    int randomNum = rand.nextInt((max - min) + 1) + min;
-
-	    return randomNum;
-	}
 	
 }
