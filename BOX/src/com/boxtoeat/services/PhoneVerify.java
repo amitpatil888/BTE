db442c7c1f811a427ee9c9ed17bdab628b3cd1c8
Amit <Amit@Amit-PrimaryBTE.local>
2015-07-28 21:20:16 -0700

    phone verify

--- BOX/src/com/boxtoeat/services/PhoneVerify.java
+++ BOX/src/com/boxtoeat/services/PhoneVerify.java
@@ -1,11 +1,18 @@
 package com.boxtoeat.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.boxtoeat.helper.PhoneVerificationCodeHelper;
+import com.boxtoeat.jdbc.DatabaseConnector;
 import com.sun.jersey.multipart.FormDataParam;
 
 @Path("/phoneVerify")
@@ -21,11 +28,44 @@ public class PhoneVerify {
 {
 		if(actionType.equalsIgnoreCase("resend"))
 		{
-			
+			new PhoneVerificationCodeHelper().createVerificationCode("", Long.parseLong(telephone));
 		}
 		else
 		if(actionType.equalsIgnoreCase("verify"))
 		{
+			try
+			{
+			String code=code1+code2+code3+code4+code5+code6;
+			DatabaseConnector dbConn=new DatabaseConnector();
+			Connection conn=dbConn.getConnection();
+			
+			String stmtStr="select username from TELEPHONE_VERIFY_LIST where TELEPHONE='"+telephone+"' and VERIFICATION_CODE='"+code+"'";
+			Statement stmt=conn.createStatement();
+			boolean bexecuted=stmt.execute(stmtStr);
+				if(bexecuted)
+				{
+					ResultSet rs=stmt.getResultSet();
+					while(rs.next())
+					{
+						String verifiedUserName=rs.getString(1);
+						Connection conn2=dbConn.getConnection();
+						
+						PreparedStatement stmt2=conn2.prepareStatement("update vitals set TELEPHONE_VERIFIED=? where TELEPHONE=?");
+						stmt2.setString(1, "Y");
+						stmt2.setLong(2, Long.parseLong(telephone));
+						int executed2Count=stmt2.executeUpdate();
+						if(executed2Count>0)
+						{
+							 return Response.status(200).entity("Thank you for verifying your phone number").build();
+						}
+						
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
 			
 		}
 		
